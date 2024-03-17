bd581656878838959f3e780b5f12a10cf5435f70
Doodlinka <oleshko.darya@gmail.com>
2024-03-17 20:26:16 +0200

    fail to figure out how to store all the data
    lots of thoughts in the comments

--- asmimpl/main.asm
+++ asmimpl/main.asm
@@ -1,17 +1,29 @@
+; this is documentt in *the book* (switches syntax from MASM to TASM), but doesn't actually work??
 ; ideal
-.model small
+.model compact
 
-; struc DataPoint
-; 	id db 16 dup ?
-; 	sumOrAverage dd ?
-; 	count dw ?
-; ends DataPoint
+MAX_ID_LEN equ 16
 
+DataPoint struc
+	id db 16 dup(?)
+	sumOrAverage dd ?
+	count dw ?
+DataPoint ends
+
+; TODO: I'll have to define two or three extra segments to store the worst-case memory requirement
+; model compact may be crucial because it allows several data segments, but apparently it's possible to not go by a model entirely?
+; each segment can fit 4096 IDs or 2978 full structs
+; refer to pages 21-22 and 447-461 of *the book* for details 
+; https://archive.org/details/bitsavers_borlandtureringTurboAssembler2ed1995_80572557/page/446/mode/2up?view=theater
 .data
 	id_buffer db 16 dup(?)
 	bxFor3F dw 0
 	cxFor3F dw 1
 	input_buffer db ?
+	data_arr DataPoint 2500 dup(<>)
+	data_arr2 DataPoint 2500 dup(<>)
+	data_arr3 DataPoint 2500 dup(<>)
+	data_arr4 DataPoint 2500 dup(<>)
 
 .stack 10h
 
