e9d8ebdf2ceaa5a1bbc144a07a719cbd5fa51fd5
Doodlinka <62027005+Doodlinka@users.noreply.github.com>
2024-03-28 15:23:09 +0200

    add more tests

--- v3test/testasm.bat
+++ v3test/testasm.bat
@@ -2,3 +2,4 @@
 ..\asmimpl\main.exe < ..\v3test\IN02.txt > ..\v3test\OUT02.txt
 ..\asmimpl\main.exe < ..\v3test\IN03.txt > ..\v3test\OUT03.txt
 ..\asmimpl\main.exe < ..\v3test\IN04.txt > ..\v3test\OUT04.txt
+..\asmimpl\main.exe < ..\v3test\IN05.txt > ..\v3test\OUT05.txt
--- v3test/verify_output.bat
+++ v3test/verify_output.bat
@@ -1,3 +1,6 @@
 ..\v3test\md5sums.exe ..\v3test\OUT01.txt ..\v3test\SAMPLE_OUT01.txt
 ..\v3test\md5sums.exe ..\v3test\OUT02.txt ..\v3test\SAMPLE_OUT02.txt
+..\v3test\md5sums.exe ..\v3test\OUT03.txt ..\v3test\SAMPLE_OUT03.txt
+..\v3test\md5sums.exe ..\v3test\OUT04.txt ..\v3test\SAMPLE_OUT04.txt
+..\v3test\md5sums.exe ..\v3test\OUT05.txt ..\v3test\SAMPLE_OUT05.txt
 pause
